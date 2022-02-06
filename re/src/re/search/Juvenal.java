8bfa29460063bde098dbef4b3c0d03ac0a869a8b
Jinmin Hu <jinmin.hu@gmail.com>
2022-02-06 23:56:32 +0100

    refactor in refactor

--- re/src/re/search/Juvenal.java
+++ re/src/re/search/Juvenal.java
@@ -22,7 +22,6 @@ import re.use.Appl;
 import re.use.Helper;
 
 public class Juvenal extends ASTVisitor implements Appl {
-    private HashMap<String, String> dynamic = new HashMap<>();
 	private IASTTranslationUnit model;
 	private JuvenateVisitor visitor;
 	private ASTRewrite rewriter;
@@ -47,7 +46,7 @@ public class Juvenal extends ASTVisitor implements Appl {
     @Override
     public int visit(IASTStatement statement) {
     	if (statement instanceof ICPPASTCompoundStatement block) {
-        	replace(block);
+        	visitor.refactor.replace(block);
 //        	remove(statement);
 //			
 //		}else {
@@ -56,104 +55,6 @@ public class Juvenal extends ASTVisitor implements Appl {
     	return super.visit(statement);
     }
 
-	private void replace(IASTCompoundStatement parent) {
-		
-		var statements = parent.getChildren();
-		
-		for(var replacement : visitor.refactor.replacements) {
-			var finds = replacement.getThenClause().getChildren();
-			var newStats = replacement.getElseClause().getChildren();
-			if(statements.length<finds.length) {
-				break;
-			}
-			try {	
-			for (int i = 0; i < statements.length; i++) {
-				var matchAll= true;
-				for (int j = 0; j < finds.length; j++) {
-					if(!compareNode(finds[j], statements[i+j] )){ 
-						matchAll =false;
-						break;
-					}
-				}
-				if(matchAll) {
-					IASTNode instert =null;
-					if(statements.length<+finds.length) {
-						instert =statements[i+finds.length];
-					}
-					for (int j = 0; j < newStats.length; j++) {
-						var text = newStats[j].getRawSignature();
-						for (var entry : dynamic.entrySet()) {
-							text = text.replace(entry.getKey(),entry.getValue());
-						}
-						rewriter.insertBefore(parent, instert,rewriter.createLiteralNode(text), null); // ((statement[i+j], null);
-					}	
-					for (int j = 0; j < finds.length; j++) {
-						rewriter.remove(statements[i+j], null);
-					}
-					i+=finds.length;
-				}
-			}
-			}catch(Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
-    private boolean compareNode(IASTNode ref,  IASTNode target) {
-    	var refChildren = ref.getChildren();
-    	var targetChildren = target.getChildren();
-    	if(refChildren.length != targetChildren.length) {
-    		return false;
-    	}
-        for (int i = 0; i < targetChildren.length; i++) {
-			if(!compareNode(refChildren[i], targetChildren[i])){
-				return false;
-			}
-        }
-
-        if(targetChildren.length==0) {
-        	var refString = ref.toString();
-        	var targetString = target.toString();
-        	 
-        	if(refString.startsWith("$")) {
-        		if(!dynamic.containsKey(refString)) {
-        			dynamic.put(refString,targetString);
-        		}
-        		return dynamic.get(refString).equals(targetString);
-        	}else {
-        		return refString.equals(targetString);        		
-        	}
-    	}
-        return true;
-    }
-
-    public static String getNode(IASTNode node) {
-        StringBuilder output = new StringBuilder();
-        if (node != null) {
-            processNode(output, "", node);
-        }
-        return output.toString();
-    }
-
-
-    private static void processNode(StringBuilder output, String indent, IASTNode node) {
-        String raw = node.getRawSignature();
-        String[] rawLines = raw.split(System.lineSeparator());
-
-        output.append(indent + "(" + node.getClass().getSimpleName() + ", " + node.getFileLocation() + "):");
-        if (rawLines.length < 2) {
-            output.append(" |" + raw + "|");
-        } else {
-            for (String line : rawLines) {
-                output.append(System.lineSeparator() + indent + "    |" + line + "|");
-            }
-        }
-        output.append(System.lineSeparator());
-
-        for (IASTNode child : node.getChildren()) {
-            processNode(output, indent + "  ", child);
-        }
-    }
 
 	public void setRewriter(ASTRewrite rewrite) {
 		rewriter =rewrite;
--- re/src/re/search/RefactorVisitor.java
+++ re/src/re/search/RefactorVisitor.java
@@ -1,12 +1,17 @@
 package re.search;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.eclipse.cdt.core.dom.ast.ASTVisitor;
+import org.eclipse.cdt.core.dom.ast.IASTCompoundStatement;
+import org.eclipse.cdt.core.dom.ast.IASTNode;
 import org.eclipse.cdt.core.dom.ast.IASTStatement;
+import org.eclipse.cdt.core.dom.ast.cpp.ICPPASTCompoundStatement;
 import org.eclipse.cdt.core.dom.ast.cpp.ICPPASTFunctionCallExpression;
 import org.eclipse.cdt.core.dom.ast.cpp.ICPPASTIfStatement;
+import org.eclipse.cdt.core.dom.rewrite.ASTRewrite;
 
 public class RefactorVisitor extends ASTVisitor {
 	public RefactorVisitor() {
@@ -16,15 +21,12 @@ public class RefactorVisitor extends ASTVisitor {
 	public List<ICPPASTIfStatement> replacements = new ArrayList<>();
 	List<ICPPASTIfStatement> removements= new ArrayList<>();
 	List<ICPPASTFunctionCallExpression> custom = new ArrayList<>();
+	private HashMap<String, String> dynamic = new HashMap<>();
 
 	@Override
 	public int visit(IASTStatement statement) {
 		if (statement instanceof ICPPASTIfStatement ifStatement) {
-			if(ifStatement.getElseClause()!=null) {
 				replacements.add(ifStatement);
-			}else {
-				removements.add(ifStatement);
-			}
 		}
 		else if (statement instanceof ICPPASTFunctionCallExpression fun) {
 			custom.add(fun);
@@ -32,4 +34,128 @@ public class RefactorVisitor extends ASTVisitor {
 		}
 		return super.visit(statement);
 	}
+
+	public void replace(IASTCompoundStatement block, ASTRewrite rewrite) {
+		for (var replacement : replacements) {
+			var start = 0;
+			var totalStatement = block.getChildren().length;
+			int seachLength = replacement.getThenClause().getChildren().length;
+			while (start >= 0 && start + seachLength <= totalStatement) {
+				start = containNode(replacement.getThenClause(), block, start);
+				if (start >= 0) {
+					if(replacement.getElseClause()!=null) {
+						replaceNewStatements(rewrite, block, replacement.getElseClause().getChildren(), start + 1);
+					}
+					removeStatement(rewrite, replacement.getThenClause().getChildren());
+				}
+				dynamic.clear();
+			}
+		}
+	}
+
+	private void removeStatement(ASTRewrite rewriter, IASTNode[] finds) {
+		for (int j = 0; j < finds.length; j++) {
+//			rewrite.remove(statements[start - j], null);
+			String text = replaceDynanic(finds[j]);
+			rewriter.remove(rewriter.createLiteralNode(text), null);
+		}
+	}
+
+	private void replaceNewStatements(ASTRewrite rewriter, IASTCompoundStatement parent, IASTNode[] newStats, int insertOffset) {
+			IASTNode instert = null;
+			if (parent.getChildren().length < insertOffset) {
+				instert = parent.getChildren()[insertOffset];
+			}
+
+			for (int j = 0; j < newStats.length; j++) {
+				String text = replaceDynanic(newStats[j]);
+				rewriter.insertBefore(parent, instert, rewriter.createLiteralNode(text), null);
+			}
+		}
+
+	private String replaceDynanic(IASTNode newStat) {
+		var text = newStat.getRawSignature();
+		for (var entry : dynamic.entrySet()) {
+			text = text.replace(entry.getKey(), entry.getValue());
+		}
+		return text;
+	}
+
+
+
+	private int containNode(IASTNode ref,  IASTNode target,int offset) {
+    	var refChildren = ref.getChildren();
+    	var targetChildren = target.getChildren();
+    	if(offset+refChildren.length > targetChildren.length) {
+    		return -1;
+    	}
+    	int j=0;
+        for (int i = offset; i < targetChildren.length; i++) {
+			if(compareNode(refChildren[j], targetChildren[i])){
+				j++;
+				if(j==refChildren.length){
+					return i;
+				}
+			}else{
+				j=0;
+			}
+        }
+        return -1;
+    }
+
+	private boolean compareNode(IASTNode ref,  IASTNode target) {
+		var refChildren = ref.getChildren();
+		var targetChildren = target.getChildren();
+		if(refChildren.length != targetChildren.length) {
+			return false;
+		}
+		for (int i = 0; i < targetChildren.length; i++) {
+			if(!compareNode(refChildren[i], targetChildren[i])){
+				return false;
+			}
+		}
+
+		if(targetChildren.length==0) {
+			var refString = ref.toString();
+			var targetString = target.toString();
+
+			if(refString.startsWith("$")) {
+				if(!dynamic.containsKey(refString)) {
+					dynamic.put(refString,targetString);
+				}
+				return dynamic.get(refString).equals(targetString);
+			}else {
+				return refString.equals(targetString);
+			}
+		}
+		return true;
+	}
+    public static String getNode(IASTNode node) {
+        StringBuilder output = new StringBuilder();
+        if (node != null) {
+            processNode(output, "", node);
+        }
+        return output.toString();
+    }
+
+
+    private static void processNode(StringBuilder output, String indent, IASTNode node) {
+        String raw = node.getRawSignature();
+        String[] rawLines = raw.split(System.lineSeparator());
+
+        output.append(indent + "(" + node.getClass().getSimpleName() + ", " + node.getFileLocation() + "):");
+        if (rawLines.length < 2) {
+            output.append(" |" + raw + "|");
+        } else {
+            for (String line : rawLines) {
+                output.append(System.lineSeparator() + indent + "    |" + line + "|");
+            }
+        }
+        output.append(System.lineSeparator());
+
+        for (IASTNode child : node.getChildren()) {
+            processNode(output, indent + "  ", child);
+        }
+    }
+
 }
