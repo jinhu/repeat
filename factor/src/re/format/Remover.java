943fe45357801694a4705d8450c0abc0ade5dfb9
jinhu <jinmin.hu@gmail.com>
2022-02-26 14:52:23 +0100

    refactor test replacement

--- factor/src/re/format/Remover.java
+++ factor/src/re/format/Remover.java
@@ -1,24 +1,20 @@
 package re.format;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.ProcessBuilder.Redirect;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-
-import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
-
-import re.use.CppSourceVisitor;
+import java.nio.file.StandardOpenOption;
+import java.util.concurrent.TimeUnit;
 
 public class Remover {
 
-	public static void main(String[] args) throws IOException {
-	
-	    try (var files = Files.walk(Path.of("C:/sw-dev/demo/voctir"))) {
-	        files.forEach((path) -> processFile(path));
-	        
-	    }
-	}
-
-	private static Object processFile(Path path) {
+	private static void processFile(Path path) {
 		if(path.toString().endsWith("_ccmwaid.inf")) {
 			System.out.println(path);
 			try {
@@ -28,7 +24,147 @@ public class Remover {
 				e.printStackTrace();
 			}
 		}
-		return null;
 	}
 
+
+
+ String sourceBase = "/mnt/c/sw-dev/demo/voctir/";
+ String targetBase = "/mnt/c/sw-dev/demo/voctir/";
+ String sourceComponent = "recv";
+ String targetComponent = "receive_";
+private File workDir;
+private Process process;
+
+ public Remover(String... args) {
+     if (args.length == 4) {
+         sourceBase = args[0];
+         sourceComponent = args[1];
+
+         targetBase = args[2];
+         targetComponent = args[3];
+     }
+ }
+
+ // @precondition: register the new component so that it can be build
+ // @postcondition: the component is cloned
+ public static void main(String[] args) throws IOException, InterruptedException, URISyntaxException {
+     var cloner = new Remover(args);
+     cloner.copy();	
+     if(cloner.test()) {
+    	 cloner.commit();
+     }else{
+    	 cloner.revert();
+     }
+ }
+
+ private void revert() throws IOException {
+     workDir = new File(sourceBase);
+     process = new ProcessBuilder("git reset --hard").directory(workDir).redirectOutput(Redirect.INHERIT).redirectError(Redirect.INHERIT).start();
+     
+}
+
+private void commit() throws IOException, InterruptedException {
+	workDir = new File(sourceBase);
+    process = new ProcessBuilder("git","add",".").directory(workDir).redirectOutput(Redirect.INHERIT).redirectError(Redirect.INHERIT).start();
+    process.waitFor(2, TimeUnit.SECONDS);
+    process = new ProcessBuilder("git","commit","-am", "'auto refactor'").directory(workDir).redirectOutput(Redirect.INHERIT).redirectError(Redirect.INHERIT).start();
+    process.waitFor(2, TimeUnit.SECONDS);
+    
+}
+
+private boolean test() throws IOException, InterruptedException {
+    System.out.println("start test");
+    workDir = new File(targetBase+targetComponent);//.replaceFirst("_$", "");
+    process = new ProcessBuilder("make").directory(workDir).redirectOutput(Redirect.INHERIT).redirectError(Redirect.INHERIT).start();
+
+	 System.out.println("finish test");
+     process.waitFor(2, TimeUnit.SECONDS);
+	 return process.exitValue() >=0;
+}
+
+private void copy() throws IOException {
+     cloneComponent(sourceBase, sourceComponent, targetBase, targetComponent);
+     updateHeaders(sourceBase+"include/", sourceComponent, targetComponent);
+}
+
+ public static void cloneEntity(String sourceBase, String sourceComponent, String targetBaseDir,
+                                String targetComponent, Path source) {
+
+     // compose the target path both the component as well as fc needs to be replaced
+	 var path =source.toString()
+	 .replace(sourceBase, targetBaseDir)
+	 .replace(sourceComponent, targetComponent);
+	// .replace("_.", ".")
+	// .replace("_/", "/")
+	// .replaceFirst("_$", "");
+     var target = Path.of(path);
+
+     // continue with cloning when one entity fails for what ever reason
+     try {
+         if (Files.isSymbolicLink(source)) {
+             //only clone symlinked csv files for now
+             if (source.toString().endsWith(".csv")) {
+                 // make sure the working dir is correct, otherwise sym lnk will fail
+                 System.setProperty("user.dir", target.getParent().toString());
+                 // compose symbolic link source
+                 var linksource = Path.of(Files.readSymbolicLink(source).toString().replace(sourceComponent, targetComponent));
+                 Files.createSymbolicLink(target, linksource);
+             }
+
+         } else if (Files.isDirectory(source)) {
+             Files.createDirectory(target);
+//				return; // just create new dir
+         } else {
+             // compose new content
+             var content = cloneContent(source, sourceComponent, targetComponent);
+             System.setProperty("user.dir", source.getParent().toString());
+             Files.write(target, content, StandardOpenOption.CREATE_NEW);
+
+         }
+     } catch (Exception e) {
+         System.out.println("skipped entry: " + source);
+     }
+ }
+
+ private static byte[] cloneContent(Path path, String sourceComponent, String targetComponent) throws IOException {
+     // text content, let's replace
+     if (!path.toString().endsWith(".so") && !path.toString().endsWith(".ddb") && !path.toString().endsWith(".pyc")
+             && !path.toString().endsWith(".pye") && !path.toString().endsWith(".vpi")
+             && !path.toString().endsWith(".a") && !path.toString().endsWith(".o")) {
+         var content = Files.readString(path);
+         content = content.replace(sourceComponent, targetComponent);
+
+         // apply exceptions in makefiles if needed
+         if (path.toString().endsWith("akefile")) {
+             // TODO: specific constrains
+         }
+         return content.getBytes();
+     } else {
+         // just return the blob
+         return Files.readAllBytes(path);
+     }
+ }
+
+
+ //clone all entries within the component
+ public void cloneComponent(String sourceBase, String sourceComponent, String targetBaseDir, String targetComponent)
+         throws IOException {
+     System.out.println("start cloning");
+
+
+     try (var files = Files.walk(Path.of(sourceBase + sourceComponent))) {
+         files.forEach((path) -> cloneEntity(sourceBase, sourceComponent, targetBaseDir, targetComponent, path));
+     }
+     System.out.println("finished cloning");
+}
+ public void updateHeaders(String headerBase, String sourceComponent, String targetComponent)
+         throws IOException {
+     System.out.println("start Header");
+
+
+     try (var files = Files.walk(Path.of(headerBase))) {
+         files.forEach((path) -> cloneEntity(headerBase, sourceComponent, headerBase, targetComponent, path));
+     }
+     System.out.println("finished Header");
+}
 }
--- factor/src/re/format/Renewer.java
+++ factor/src/re/format/Renewer.java
@@ -3,84 +3,159 @@ package re.format;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.ProcessBuilder.Redirect;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
-
-import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
-import org.eclipse.text.edits.ReplaceEdit;
-
-import re.use.CppSourceVisitor;
 
 public class Renewer {
 
-	public static void main(String[] args) throws IOException, InterruptedException {
-		//auto TCR
-		var target = "/mnt/c/sw-dev/demo/voctir";
-	    try (var files = Files.walk(Path.of(target))) {
-	        files.forEach((path) -> processFile(path));
-	        
-	    }
-	    Process process = Runtime.getRuntime().exec("/usr/bin/make", null, new File(target));
-	    //
-      BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
-      StringBuilder result = new StringBuilder();
-      String line;
-      while ((line = in.readLine()) != null) {
-              System.out.println(line);
-      }
-        process.waitFor(300, TimeUnit.SECONDS);
-	    
-      if (!String.valueOf(process.exitValue()).matches("[01]")) {      
-        process = Runtime.getRuntime().exec("git reset --hard", null, new File(target));
-        process.waitFor(60, TimeUnit.SECONDS);
-      }else {
-       process = Runtime.getRuntime().exec("git commit -am'auto refactor ...'", null, new File(target));
-       process.waitFor(60, TimeUnit.SECONDS);
+	String sourceBase = "/mnt/c/sw-dev/demo/voctir/";
+	String targetBase = "/mnt/c/sw-dev/demo/voctir/";
+	String sourceComponent = "recv";
+	String targetComponent = "receive_";
+	private File workDir;
+	private Process process;
 
-      }
+	public Renewer(String... args) {
+		if (args.length == 4) {
+			sourceBase = args[0];
+			sourceComponent = args[1];
+
+			targetBase = args[2];
+			targetComponent = args[3];
+		}
 	}
-	static int i=0;
-	private static void processFile(Path path) {
-		if(path.toString().endsWith(".c")) {
-			try {
-				System.out.println("file: "+ (i++) + "\t "+path);
-				var text = Files.readString(path);
-				text = text.replace('', ' ')
-//						.replaceAll(";\\+\\+", "")
-//						.replaceAll("\\n;\\s*\\n", "\n")
-						.replaceAll("; \\$Id:.*\\$","")
-						.replaceAll("; \\$Log:.*\\$","")
-						.replaceAll(";\\n;\\s+\\(c\\) .*\\d+\\n;\\s+All rights reserved.\\n;\\n", " (c) Example B.V. 2022   All rights reserved.\n");
-				text = text.replaceAll(";  FUNCTIONAL DESCRIPTION:\\n;  (.*)", "/// $1");
-				text = text.replaceAll(";  PRECONDITIONS:\n;  (.*)", "@Preconditions $1");
-				text = text.replaceAll(";  KNOWN LIMITATIONS:\n;  (.*)","@Limitations $1");
-				text = text.replaceAll(";  (\\S+)    (.*)","@param $1\t$2");
-				
-				text = text.replaceAll("static\\s+(\\S+) ?\\(", "int $1(");
-				text = text.replaceAll("static\\s+(\\S+)\\s+(\\S+) ?\\(", "$1 $2(");
-				text = text.replaceAll("extern int	errno;", "#include <errno.h>\nextern int	errno;");
-				text = text.replaceAll("extern char.*sys_errlist\\[\\];", "");				
-				Files.write(path, text.getBytes());
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+
+	public static void main(String[] args) throws IOException, InterruptedException, URISyntaxException {
+
+		var renewer = new Renewer(args);
+		renewer.run(false);
+		if (renewer.test()) {
+			renewer.commit();
+		} else {
+			renewer.revert();
+		}
+	}
+
+	private boolean revert() throws IOException {
+		workDir = new File(sourceBase);
+		process = new ProcessBuilder("git reset --hard").directory(workDir).redirectOutput(Redirect.INHERIT)
+				.redirectError(Redirect.INHERIT).start();
+		return process.exitValue() >= 0;
+	}
+
+	private boolean commit() throws IOException, InterruptedException {
+		workDir = new File(sourceBase);
+		process = new ProcessBuilder("git", "add", ".").directory(workDir).redirectOutput(Redirect.INHERIT)
+				.redirectError(Redirect.INHERIT).start();
+		process.waitFor(2, TimeUnit.SECONDS);
+		process = new ProcessBuilder("git", "commit", "-am", "'auto refactor'").directory(workDir)
+				.redirectOutput(Redirect.INHERIT).redirectError(Redirect.INHERIT).start();
+		process.waitFor(2, TimeUnit.SECONDS);
+		return process.exitValue() >= 0;
+	}
+
+	private boolean test() throws IOException, InterruptedException {
+		System.out.println("start test");
+		workDir = new File(targetBase + targetComponent);// .replaceFirst("_$", "");
+		process = new ProcessBuilder("make").directory(workDir).redirectOutput(Redirect.INHERIT)
+				.redirectError(Redirect.INHERIT).start();
+
+		System.out.println("finish test");
+		process.waitFor(2, TimeUnit.SECONDS);
+		return process.exitValue() >= 0;
+	}
+
+	private void run(boolean finalRun) throws IOException {
+			var files = Files.walk(Path.of(sourceBase + sourceComponent));
+			files.forEach(this::cloneEntity);
+			if(finalRun) files.forEach(this::delete);
+			files = Files.walk(Path.of(sourceBase + "include/"));
+			files.forEach(this::cloneEntity);
+			if(finalRun) files.forEach(this::delete);
+	}
+
+	private void cloneEntity(Path source) {
+		try {
+			var target = composeTargetPath(source);
+			switch (ContentTypeOf(source)) {
+				case Link:  	cloneLink(source, target); 		break;
+				case Directory:	Files.createDirectory(target); 	break;
+				case Binary:	Files.copy(source, target); 	break;
+				case Content:	cloneContent(source, target); 	break;
+				case Garbage:   Files.delete(source); 			break;
 			}
-			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	private Path composeTargetPath(Path source) {
+		var path = source.toString().replace(sourceBase, targetBase).replace(sourceComponent, targetComponent);
+		// .replace("_.", ".").replace("_/", "/").replaceFirst("_$", "");
+		var target = Path.of(path);
+		return target;
+	}
+
+	private ContentType ContentTypeOf(Path source) {
+		var name = source.toString();
+		var extension = name.substring(name.lastIndexOf(".")+1);
+		
+		if(Files.isSymbolicLink(source)) 		return ContentType.Directory;
+		if(Files.isDirectory(source)) 			return ContentType.Directory;
+		if(name.endsWith("_ccmwaid.inf")) 		return ContentType.Garbage;
+		if(binaryExtension.contains(extension)) return ContentType.Binary;
+												return ContentType.Content;
+	}
+	enum ContentType {
+		Directory, Link, Binary, Content, Garbage
+	}
+	List<String> binaryExtension = List.of("so","ddb","pyc","pye","vpi","a","o");
+
+	private void cloneContent(Path source, Path target) throws IOException {
+			var text = Files.readString(source);
+			text = text.replace(sourceComponent, targetComponent);
+			text = text.replace('', ' ').replaceAll(";\\+\\+", "").replaceAll("\\n;\\s*\\n", "\n")
+					.replaceAll("; \\$Id:.*\\$", "").replaceAll("; \\$Log:.*\\$", "")
+					.replaceAll(";\\n;\\s+\\(c\\) .*\\d+\\n;\\s+All rights reserved.\\n;\\n",
+							" (c) Example B.V. 2022   All rights reserved.\n");
+			text = text.replaceAll(";  FUNCTIONAL DESCRIPTION:\\n;  (.*)", "/// $1");
+			text = text.replaceAll(";  PRECONDITIONS:\n;  (.*)", "@Preconditions $1");
+			text = text.replaceAll(";  KNOWN LIMITATIONS:\n;  (.*)", "@Limitations $1");
+			text = text.replaceAll(";  (\\S+)    (.*)", "@param $1\t$2");
+
+			text = text.replaceAll("static\\s+(\\S+) ?\\(", "int $1(");
+			text = text.replaceAll("static\\s+(\\S+)\\s+(\\S+) ?\\(", "$1 $2(");
+			text = text.replaceAll("extern int	errno;", "#include <errno.h>\nextern int	errno;");
+			text = text.replaceAll("extern char.*sys_errlist\\[\\];", "");
+
+			// apply exceptions in makefiles if needed
+//			if (source.toString().endsWith("akefile")) {
+//			}
+			Files.write(target, text.getBytes(), StandardOpenOption.CREATE_NEW);
+
+	}
+
+
+	private void cloneLink(Path source, Path target) throws IOException {
+		// make sure the working dir is correct, otherwise sym lnk will fail
+		System.setProperty("user.dir", target.getParent().toString());
+		// compose symbolic link source
+		var linksource = Path.of(Files.readSymbolicLink(source).toString().replace(sourceComponent, targetComponent));
+		Files.createSymbolicLink(target, linksource);
+	}
+
+	private void delete(Path path) {
+		try {
+			Files.delete(path);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
 }
-//        BufferedReader err = new BufferedReader(new InputStreamReader(process.getInputStream()));
-//        StringBuilder errResult = new StringBuilder();
-//        String errLine;
-//        while ((errLine = err.readLine()) != null) {
-//            System.out.println(errLine);
-//            errResult.append(errLine).append(System.lineSeparator());
-//        }
-//        process.waitFor(5, TimeUnit.SECONDS);
-//        if (!ignoreError && !String.valueOf(process.exitValue()).matches("[01]")) {
-//            throw new Exception(
-//                    "Error: " + process.exitValue() + " " + errResult + " " + result);
-//        }
