9ea886614ee51811769556d9421c43bb247e0b8b
jinhu <jinmin.hu@gmail.com>
2022-03-20 22:52:50 +0100

    split plugin related code from cdt code

--- view/src/re/view/RefactorHandler.java
+++ view/src/re/view/RefactorHandler.java
@@ -1,39 +1,80 @@
 package re.view;
 
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
+import org.eclipse.cdt.core.dom.ast.ASTVisitor;
+import org.eclipse.cdt.core.dom.ast.IASTNode;
+import org.eclipse.cdt.core.dom.ast.IASTPreprocessorIfdefStatement;
+import org.eclipse.cdt.core.dom.ast.IASTStatement;
 import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
+import org.eclipse.cdt.core.dom.ast.cpp.ICPPASTCompoundStatement;
+import org.eclipse.cdt.core.dom.rewrite.ASTRewrite;
 import org.eclipse.cdt.core.model.CoreModel;
+import org.eclipse.cdt.core.model.ICElement;
+import org.eclipse.cdt.core.model.ICElementVisitor;
+import org.eclipse.cdt.core.model.ITranslationUnit;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.ui.ISelectionService;
-import org.eclipse.ui.IWorkbench;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.common.util.BasicMonitor;
+import org.eclipse.emf.common.util.BasicMonitor.Printing;
 import org.eclipse.ui.IWorkbenchPart;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
 
-
 import re.factor.Helper;
-import re.factor.RefactorVisitor;
 import re.factor.Refactorings;
 
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.text.TextSelection;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
-
-public class RefactorHandler extends AbstractHandler {
+public class RefactorHandler extends AbstractHandler implements ICElementVisitor{
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		
-		
-//		String content = IOUtils.toString(file.getContents(), file.getCharset());
-		
+		try {
+			refactorings = getRefactoring(event);
+			CoreModel.getDefault().getCModel().accept(this);
+		} catch (CoreException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	Refactorings refactorings;
+	ASTRewrite rewrite;
+	
+	protected IProgressMonitor progressMonitor = BasicMonitor.toIProgressMonitorWithBlocking(new Printing(System.out));
+
+	@Override
+	public boolean visit(ICElement element) throws CoreException {
+		if(element instanceof ITranslationUnit tu) {
+			var atu = tu.getAST();
+            if(atu!=null) {
+        		rewrite = ASTRewrite.create(atu);
+                atu.accept(visitor);
+            	var change = rewrite.rewriteAST();
+            	change.perform(progressMonitor);
+			}
+		}
+		return true;
+	}
+
+	ASTVisitor visitor = new ASTVisitor(true) {
+        @Override
+        public int visit(IASTStatement statement) {
+        	if (statement instanceof ICPPASTCompoundStatement block) {
+	        		refactorings.process(rewrite, block);
+			} 
+        	return super.visit(statement);    	
+        }  
+    };
+    
+	
+	private Refactorings getRefactoring(ExecutionEvent event) throws ExecutionException {
 		var window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
 		var selectionService = window.getSelectionService();
 		IWorkbenchPart workbenchPart = window.getActivePage().getActivePart(); 
@@ -44,16 +85,6 @@ public class RefactorHandler extends AbstractHandler {
 		var selection = selectionService.getSelection();
 		var refactorings = new Refactorings();
 		atu.accept(refactorings);
-
-		var visitor = new RefactorVisitor();
-		visitor.setRefactorings(refactorings);
-		
-		try {
-			CoreModel.getDefault().getCModel().accept(visitor);
-		} catch (CoreException e) {
-			e.printStackTrace();
-		}
-
-		return null;
+		return refactorings;
 	}
 }
